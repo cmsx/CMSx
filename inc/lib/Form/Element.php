b16ca69f22de287b5074ea621ad95577d534a327
Igor Osharin <igor@osharin.ru>
2012-08-12 13:59:44 +0400

    CMSx-3. Базовый класс для отдельного элемента формы и тесты к нему.

---
+++ inc/lib/Form/Element.php
@@ -0,0 +1,295 @@
+<?php
+
+abstract class FormElement
+{
+  protected $id;
+  protected $name;
+  protected $info;
+  protected $label;
+  protected $field;
+  protected $value;
+  protected $regexp;
+  protected $filter;
+  protected $errors;
+  protected $default;
+  protected $attribute;
+  protected $form_name;
+  protected $is_hidden;
+  protected $placeholder;
+  protected $is_required;
+  protected $label_as_placeholder;
+
+  /** Шаблон для отрисовки инпута. Порядок параметров: id, name, attribute, value */
+  protected $tmpl_input = '<input id="%s" name="%s" type="text"%s value="%s" />';
+  /** Шаблон для доп.информации по полю */
+  protected $tmpl_info = '<em>%s</em>';
+  /** Шаблон для строки содержащей все строки ошибок */
+  protected $tmpl_errs = '<span class="error">%s</span><br />';
+  /** Шаблон для одной строки в ошибках */
+  protected $tmpl_err = '%s<br />';
+  /** Шаблон обязательно поле не заполнено */
+  protected $tmpl_err_required = 'Обязательное поле "%s" не заполнено';
+  /** Шаблон ошибки по регулярному выражению */
+  protected $tmpl_err_regexp = 'Поле "%s" заполнено некорректно';
+  /** Шаблон ошибки по фильтру */
+  protected $tmpl_err_filter = 'Поле "%s" заполнено некорректно';
+  /** Шаблон для обязательного поля */
+  protected $tmpl_required = '<span>*</span>';
+
+  function __construct($field, $label = null, $form_name = null)
+  {
+    $this->field     = $field;
+    $this->name      = $field;
+    $this->id        = $field;
+    $this->label     = $label;
+    $this->form_name = $form_name;
+    $this->init();
+  }
+
+  /** Отрисовка блока с полем целиком */
+  public function render()
+  {
+    return '<label'.($this->hasErrors() ? ' class="error_label"' : '').'>'
+      .$this->renderErrors().$this->renderIsRequired().$this->getLabel()
+      .' '.$this->getInput().'</label>';
+  }
+
+  /** Отрисовка поля формы */
+  public function renderInput()
+  {
+    return sprintf($this->tmpl_input, $this->getId(), $this->getName(), $this->renderAttribute(), $this->getValue());
+  }
+
+  /** Отрисовка ошибок для поля */
+  public function renderErrors()
+  {
+    if (!$this->hasErrors()) {
+      return '';
+    } else {
+      $out = '';
+      foreach ($this->errors as $str) {
+        $out .= sprintf($this->tmpl_err, $str);
+      }
+      return sprintf($this->tmpl_errs, $out);
+    }
+  }
+
+  /** Отрисовка признака обязательности заполнения поля */
+  public function renderIsRequired()
+  {
+    return $this->is_required ? $this->tmpl_required : '';
+  }
+
+  /** Отрисовка поля с доп. информацией */
+  public function renderInfo()
+  {
+    return !empty($this->info) ? sprintf($this->tmpl_info, $this->info) : '';
+  }
+
+  /** Отрисовка доп.аттрибутов поля */
+  public function renderAttribute()
+  {
+    if ($this->getPlaceholder()) {
+      $this->attribute['placeholder'] = $this->getPlaceholder();
+    }
+    $out = '';
+    if (is_array($this->attribute)) {
+      foreach ($this->attribute as $key=> $val) {
+        $out .= sprintf(' %s="%s"', $key, $val);
+      }
+    }
+    return $out;
+  }
+
+  /** Проверка значения поля */
+  public function verify($data)
+  {
+    $this->errors = null;
+    $this->value = $data;
+    if (!empty($data)) {
+      if ($this->regexp && !preg_match($this->regexp, $data)) {
+        $this->errors[] = sprintf($this->tmpl_err_regexp, $this->label);
+      }
+      if ($this->filter && !call_user_func_array($this->filter, array($data))) {
+        $this->errors[] = sprintf($this->tmpl_err_filter, $this->label);
+      }
+    } else {
+      if ($this->is_required) {
+        $this->errors[] = sprintf($this->tmpl_err_required, $this->label);
+      }
+    }
+    if ($this->hasErrors()) {
+      return false;
+    }
+    return true;
+  }
+
+  /** Проверка наличия ошибок после проверки */
+  public function hasErrors()
+  {
+    return is_array($this->errors) && count($this->errors) > 0;
+  }
+
+  /**
+   * @return array|bool
+   */
+  public function getErrors()
+  {
+    return $this->hasErrors() ? $this->errors : false;
+  }
+
+  protected function init()
+  {
+
+  }
+
+  // GETERS
+
+  /** Значение по умолчанию */
+  public function getDefault()
+  {
+    return $this->default;
+  }
+
+  /** Поле в форме */
+  public function getField()
+  {
+    return $this->field;
+  }
+
+  /** Имя поля в HTML */
+  public function getName()
+  {
+    return !empty($this->form_name)
+      ? $this->form_name.'['.$this->name.']'
+      : $this->name;
+  }
+
+  /** Имя поля для людей при отрисовке */
+  public function getLabel()
+  {
+    return !is_null($this->label) ? $this->label : ucfirst($this->field);
+  }
+
+  /** Тег ID для поля */
+  public function getId()
+  {
+    return 'form-'.(!empty($this->form_name) ? $this->form_name.'-' : '').$this->id;
+  }
+
+  /** Тег placeholder для поля */
+  public function getPlaceholder()
+  {
+    return !empty($this->placeholder)
+      ? $this->placeholder
+      : ( $this->label_as_placeholder
+        ? $this->label
+        : '' );
+  }
+
+  /**
+   * Значение поля формы
+   * @param bool $clean - экранировать ли вывод
+   */
+  public function getValue($clean = true)
+  {
+    return $clean ? htmlspecialchars($this->value) : $this->value;
+  }
+
+  public function getIsHidden()
+  {
+    return (bool)$this->is_hidden;
+  }
+
+  public function getIsRequired()
+  {
+    return (bool)$this->is_required;
+  }
+
+  // SETTERS
+
+  public function setUseLabelAsPlaceholder($val)
+  {
+    $this->label_as_placeholder = (bool)$val;
+    return $this;
+  }
+
+  public function setFormName($form_name)
+  {
+    $this->form_name = $form_name;
+    return $this;
+  }
+
+  public function setId($id)
+  {
+    $this->id = $id;
+    return $this;
+  }
+
+  public function setIsRequired($is_required)
+  {
+    $this->is_required = $is_required;
+    return $this;
+  }
+
+  public function setLabel($label)
+  {
+    $this->label = $label;
+    return $this;
+  }
+
+  public function setInfo($info)
+  {
+    $this->info = $info;
+    return $this;
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+    return $this;
+  }
+
+  public function setPlaceholder($placeholder)
+  {
+    $this->placeholder = $placeholder;
+    return $this;
+  }
+
+  public function setRegexp($regexp)
+  {
+    $this->regexp = $regexp;
+    return $this;
+  }
+
+  public function setFilter($filter)
+  {
+    if (!is_callable($filter)) {
+      throw new Exception('Фильтр не может быть вызван', 501);
+    }
+    $this->filter = $filter;
+    return $this;
+  }
+
+  public function setDefault($default)
+  {
+    $this->default = $default;
+    $this->verify($default);
+    return $this;
+  }
+
+  public function setAttribute($attribute)
+  {
+    if (is_string($attribute)) {
+      $attribute = array('class'=>$attribute);
+    }
+    $this->attribute = (array)$attribute;
+    return $this;
+  }
+
+  public function addAttribute($key, $val)
+  {
+    $this->attribute[$key] = $val;
+    return $this;
+  }
+}
---
+++ inc/test/unit/020.FormElementTest.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once __DIR__.'/../../init.php';
+class TestFormElement extends FormElement
+{
+}
+
+class FormElementTest extends PHPUnit_Framework_TestCase
+{
+  function testConstruct()
+  {
+    $el = new TestFormElement('name', 'Имя', 'myform');
+    $this->assertEquals('name', $el->getField(), 'Имя поля');
+    $this->assertEquals('Имя', $el->getLabel(), 'Название поля');
+    $this->assertEquals('myform[name]', $el->getName(), 'Тег name');
+    $this->assertEquals('form-myform-name', $el->getId(), 'Тег id');
+  }
+
+  function testRender()
+  {
+    $el = new TestFormElement('name', 'Имя');
+    $str = '<input id="form-name" name="name" type="text" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута по умолчанию');
+
+    $el->setFormName('myform');
+    $str = '<input id="form-myform-name" name="myform[name]" type="text" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с именованной формой');
+
+    $el
+      ->setUseLabelAsPlaceholder(true)
+      ->setLabel('ямИ');
+    $str = '<input id="form-myform-name" name="myform[name]" type="text" placeholder="ямИ" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с плейсхолдером из названия поля');
+
+    $el->setPlaceholder('Другое имя');
+    $str = '<input id="form-myform-name" name="myform[name]" type="text" placeholder="Другое имя" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с произвольным плейсхолдером');
+
+    $el->setAttribute('myclass');
+    $str = '<input id="form-myform-name" name="myform[name]" type="text" class="myclass" placeholder="Другое имя" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с классом и плейсхолдером');
+
+    $el
+      ->setPlaceholder(null)
+      ->setUseLabelAsPlaceholder(false)
+      ->setId('myid')
+      ->setName('amen')
+      ->setAttribute(array('size'=>10, 'class'=>'yeah'))
+      ->addAttribute('maxlength', 20);
+    $str = '<input id="form-myform-myid" name="myform[amen]" type="text" size="10" class="yeah" maxlength="20" value="" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с атрибутами');
+  }
+
+  function testDefaultValues()
+  {
+    $el = new TestFormElement('name', 'Имя');
+    $el->setFilter('is_numeric');
+    $el->setDefault(123);
+
+    $str = '<input id="form-name" name="name" type="text" value="123" />';
+    $this->assertEquals($str, $el->renderInput(), 'Создание инпута с значением по умолчанию');
+
+    $el->verify('abc');
+    $this->assertEquals(true, $el->hasErrors(), 'Есть ошибка по фильтру');
+    $str = '<input id="form-name" name="name" type="text" value="abc" />';
+    $this->assertEquals($str, $el->renderInput(), 'Инпут с значением по умолчанию и ошибкой в форме');
+  }
+
+  function testVerify()
+  {
+    $el = new TestFormElement('email', 'Email');
+    $el->setRegexp(REGULAR_EMAIL);
+
+    $el->verify(null);
+    $this->assertEquals(false, $el->hasErrors(), 'По необязательному полю ошибок нет');
+
+    $el->setIsRequired(true);
+    $el->verify(null);
+    $this->assertEquals(true, $el->hasErrors(), 'По обязательному полю есть ошибки');
+
+    $el->verify('somebody');
+    $this->assertEquals(true, $el->hasErrors(), 'По регулярному выражению не проходит');
+
+    $el->verify('my.very@lovely.email.ru');
+    $this->assertEquals(false, $el->hasErrors(), 'Корректный email 1');
+
+    $el->verify('my-very@lovely-email.ru');
+    $this->assertEquals(false, $el->hasErrors(), 'Корректный email 2');
+    $el->setRegexp(null);
+
+    try {
+      $el->setFilter('non_callable');
+      $this->fail('Фильтр должен выкидывать исключение на невызываемую функцию');
+    } catch (PHPUnit_Framework_AssertionFailedError $e) {
+      throw $e;
+    } catch (Exception $e) {}
+
+    try {
+      $el->setFilter('is_numeric');
+    } catch (Exception $e) {
+      $this->fail('Фильтр не должен выкидывать исключение на корректную функцию');
+    }
+
+    $el->verify(123);
+    $this->assertEquals(false, $el->hasErrors(), 'Ошибки по фильтру нет');
+
+    $el->verify('abc');
+    $this->assertEquals(true, $el->hasErrors(), 'Есть ошибка по фильтру');
+  }
+}
