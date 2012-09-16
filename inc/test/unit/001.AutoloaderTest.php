f6fd399303df163120ea6ccae4a632218c57c05f
Igor Osharin <igor@osharin.ru>
2012-09-16 13:47:35 +0400

    CMSx-6 Вынесение конфигурации для тестов

--- inc/test/unit/001.AutoloaderTest.php
+++ inc/test/unit/001.AutoloaderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../init.php';
+require_once __DIR__.'/../init.php';
 
 class AutoloaderTest extends PHPUnit_Framework_TestCase
 {
--- inc/test/unit/040.URLTest.php
+++ inc/test/unit/040.URLTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../init.php';
+require_once __DIR__.'/../init.php';
 
 class URLTest extends PHPUnit_Framework_TestCase
 {
