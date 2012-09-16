98052e2a60f44548457f874cfd913a54637bbec6
Igor Osharin <igor@osharin.ru>
2012-09-16 13:07:18 +0400

    CMSx-10 Базовый класс для схемы. Функционал создания таблицы.

---
+++ inc/lib/Schema.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * В конструкторе наследующего класса обязательно должны быть
+ * заданы имя таблицы и запрос для её создания через SQL::Create()...
+ */
+abstract class Schema
+{
+  /** Имя создаваемой таблицы без префикса */
+  protected $table;
+  /** @var SQLQueryCreate */
+  protected $query;
+
+  /** Настройка имени и структуры таблицы */
+  function __construct()
+  {
+
+  }
+
+  /** Создание таблицы */
+  public function createTable()
+  {
+    if (is_null($this->table)) {
+      throw new Exception(get_called_class() . ': Имя таблицы не определено', 501);
+    }
+    if (is_null($this->query)) {
+      throw new Exception(get_called_class() . ': SQL для создания таблицы не определен', 501);
+    }
+    return $this->query->execute();
+  }
+
+  /** Забивание таблицы стартовым контентом */
+  public function fillContent()
+  {
+    return true;
+  }
+
+  /** Имя таблицы в БД */
+  public function getTable()
+  {
+    return $this->table;
+  }
+}
