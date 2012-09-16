2b8dba61d2201c3c665ce2fd30172e600abdbaec
Igor Osharin <igor@osharin.ru>
2012-09-16 13:09:12 +0400

    CMSx-6 Из конфига убрано лишние константы имени системы, т.к. в дальнейшем это будет получаться из настроек в БД.

--- inc/config.php
+++ inc/config.php
@@ -6,12 +6,6 @@ new Connection('localhost', 'cmsx', 'qwerty', 'cmsx', 'utf8');
 /** Префикс к таблицам в БД */
 define ('PREFIX', 'cmsx_');
 
-/** Домен сайта без http://, если нужно, с www */
-define ('SITE_URL', 'www.cmsx.ru');
-
-/** Название сайта для человеков и писем */
-define ('SITE_NAME', 'Новый сайт');
-
 /**
  * Режим разработчика -
  * вывод ошибок PHP + расширенная информация при отображении Exception в CMS
